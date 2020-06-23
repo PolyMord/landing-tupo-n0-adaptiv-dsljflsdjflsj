c027955dd10088ba53d9684b6a52e27acbea835c
PolyMord <stanislavmalickiy@gmail.com>
2020-06-24 02:36:48 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.[chunkhash].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    port: 8000,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+}
